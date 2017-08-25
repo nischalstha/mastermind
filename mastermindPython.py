bd9eb457bedc4d670d038c5b6687f31d8ed16221
Nischal Shrestha <ns7442@truman.edu>
2017-08-24 21:11:36 -0500

    Update mastermindPython.py

--- mastermindPython.py
+++ mastermindPython.py
@@ -1,16 +1,6 @@
 #Nischal Shrestha
 #9/19/2015
-#Program 2 - MASTERMIND GAME
-
-#This program would generate 4 letter string code from the alphabet 'ABCDEF'.
-#For example, ACFE or DACB or BBCD
-#INPUT: Prompt the user to input user's guess.
-#OUTPUT: In each guess the program would print:
-    #i)how many letters that are in correct place
-    #ii)how many are correct letters.
-#The program would repeatedly prompt the user for a guess until the user guesses it right.
-#The user will have 10 chances to guess to win the game.
-#If user guesses it right, user would win. If not, computer wins.
+#MASTERMIND GAME
 
 import random
 
